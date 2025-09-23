1b20567cb8d006f98f849618520b966a77fc8b92
fendraq <fendraq@gmail.com>
2025-09-23 15:55:36 +0200

    Add script model training and data processing script for full dataset

---
+++ image_preprocessing/pln_image_konverting.py
@@ -0,0 +1,73 @@
+# For converting .jpg to numpy array
+import numpy as np
+from PIL import Image
+import os
+import polars as pl
+
+def image_size(image_path):
+    """ Return image size in pixel"""
+    with Image.open(image_path) as image:
+        return image.size
+
+def load_and_process_image(image_path, target_size):
+    """ Reading, converting to grayscale, normalize and flatten to a feature vector
+    Args: 
+        image_path (str): Path to image.
+        target_size (tuple): Expected (width, height) in pixel
+
+    returns:
+        np.ndarray: Flattened and normalized feature vector of the image
+
+    Raises:
+        ValueError: If the size of the image doesnt match target_size.
+    """
+    with Image.open(image_path) as image:
+        image_gray = image.convert('L')
+        image_array = np.array(image_gray)
+    if image_array.shape != (target_size[1], target_size[0]):
+        raise ValueError(f"Wrong size of {image_path}: {image_array.shape}, expecting {target_size[::-1]}")
+    image_normalized = image_array.astype(np.float32) / 255.0
+    feature_vector = image_normalized.flatten()
+    return feature_vector
+
+def preprocessing_image_batch(image_folder, filenames, target_size):
+    """ 
+    Converting a list of images to a feature matrix, using load_and_process_image.
+    Iterating the image folder in batches of 1000, to convert each image to to a flattened and normalized feature vector
+
+    Args:
+        image_folder (str): Path to images
+        filenames (list): Names of the files to be converted
+        target_size (tuple): width and height of image
+
+    Returns:
+        np.ndarray: Feature matrix where each row corresponds to an image 
+    
+    """
+    n_images = len(filenames)
+    n_features = target_size[0] * target_size[1]
+
+    X = np.zeros((n_images, n_features), dtype=np.float32)
+
+    for i, filename in enumerate(filenames):
+        image_path = os.path.join(image_folder, filename)
+        features = load_and_process_image(image_path, target_size)
+        X[i, :] = features
+
+        if (i + 1) % 1000 == 0:
+            print(f"Processed {i + 1}/{n_images} images")
+    return X
+
+def main():
+    df =  pl.read_parquet('../data/processed/labels/df_full_filtered_with_title.parquet')
+    filenames = df['file_name'].to_list()
+    image_folder = '../data/Pub_Lay_Net/documents'
+    first_image_path = os.path.join(image_folder, filenames[0])
+    target_size = image_size(first_image_path)
+    print(f"Target size: {target_size}")
+    X_raw = preprocessing_image_batch(image_folder, filenames, target_size)
+    np.save('../data/processed/pln_X_features_raw_original_size.npy', X_raw)
+    print("Feature matrix saved")
+
+if __name__ == '__main__':
+    main()
---
+++ pln_model_training_scripts/pln_first_page_model_128x128.py
@@ -0,0 +1,65 @@
+# Script for training ensemble model on dataset converted to 128x128
+# Imports
+import numpy as np
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing  import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+
+print("Loading data")
+X = np.load('../data/processed/pln_X_features_raw_128x128.npy')
+y = np.load('../data/processed/pln_y_labels.npy')
+
+print("Keeping top 40 pixel rows")
+X_reshape = X.reshape(-1, 128, 128)
+X_top40 = X_reshape[:, :40, :]
+X_top40_flat = X_top40.reshape(X_top40.shape[0], -1)
+
+print("Splitting data in train and test")
+X_train, X_test, y_train, y_test = train_test_split(
+    X_top40_flat, y, test_size=0.20, 
+    random_state=42,
+    stratify=y
+)
+
+print("Scaling data")
+scaler = StandardScaler()
+X_trained_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+print("Initializing models")
+knc = KNeighborsClassifier(metric='minkowski', n_neighbors=2, weights='distance')
+rfc = RandomForestClassifier(max_depth= None, min_samples_split=5, n_estimators=150)
+
+print("Training KNC")
+knc.fit(X_trained_scaled, y_train) 
+
+print("Training RFC")
+rfc.fit(X_trained_scaled, y_train)
+
+print("Setting threshold to 0.4")
+threshold = 0.4
+rfc_y_proba = rfc.predict_proba(X_test_scaled)[:, 1] 
+rfc_y_pred = (rfc_y_proba >= threshold).astype(int)
+
+knc_y_proba = knc.predict_proba(X_test_scaled)[:, 1] 
+knc_y_pred = (knc_y_proba >= threshold).astype(int)
+
+print("Testing prediction")
+ensemble_pred = np.maximum(rfc_y_pred, knc_y_pred)
+cm = confusion_matrix(y_test, ensemble_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+
+print(cm)
+print(classification_report(y_test, ensemble_pred))
+
+print("Saving models")
+joblib.dump(rfc, "../models/pln_first_page_model_128x128/rfc_model.pkl")
+print("RFC saved")
+joblib.dump(knc, "../models/pln_first_page_model_128x128/knc_model.pkl")
+print("KNC saved")
+joblib.dump(scaler, "../models/pln_first_page_model_128x128/scaler_model.pkl")
+print("Scaler saved")
